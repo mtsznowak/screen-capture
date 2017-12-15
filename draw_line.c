37664438b9c6e69b4027f7ffbda7cc45708a934f
Mateusz Nowak <mtsznowak9@gmail.com>
2017-12-15 21:51:18 +0100

    normalize coordinates before drawing

--- draw_line.c
+++ draw_line.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <X11/Xutil.h>
 
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 int main(int argc, char* argv[]) {
    if(argc != 5) {
@@ -29,6 +31,24 @@ int main(int argc, char* argv[]) {
    }
 
    s = DefaultScreen(d);
+   Screen *screen = ScreenOfDisplay(d, s);
+
+   //normalize coordinates
+   x = MIN(MAX(x, 1), screen->width-3);
+   y = MIN(MAX(y, 1), screen->height-3);
+   width=MAX(width, 1);
+   height=MAX(height, 1);
+   
+   printf("x: %d y: %d, w: %d, h: %d\n", x, y, width, height);
+   if(x+width >= screen->width-1) {
+      width = screen->width-x-2;
+   }
+   
+   if(y+height >= screen->height-1) {
+      height = screen->height-y-2;
+   }
+
+
    w = XCreateSimpleWindow(d, RootWindow(d, s), x, y, width, height, 0,
                            BlackPixel(d, s), WhitePixel(d, s));
 
